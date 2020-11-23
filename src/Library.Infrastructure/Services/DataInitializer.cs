8a2cbe3da30c2a971a76e9a6cfdf2bd6a73f3f6e
vescs <mt.kepa98@gmail.com>
2020-11-23 13:09:22 +0100

    Data seed for all current models

--- src/Library.Infrastructure/Services/DataInitializer.cs
+++ src/Library.Infrastructure/Services/DataInitializer.cs
@@ -9,9 +9,16 @@ namespace Library.Infrastructure.Services
     public class DataInitializer : IDataInitializer
     {
         private readonly IBookService _bookService;
-        public DataInitializer(IBookService bookService)
+        private readonly IEventService _eventService;
+        private readonly INewspaperService _newspaperService;
+        private readonly IMovieService _movieService;
+        public DataInitializer(IBookService bookService, IEventService eventService, 
+            INewspaperService newspaperService, IMovieService movieService)
         {
             _bookService = bookService;
+            _eventService = eventService;
+            _movieService = movieService;
+            _newspaperService = newspaperService;
         }
         public async Task SeedAsync()
         {
@@ -20,6 +27,12 @@ namespace Library.Infrastructure.Services
             {
                 tasks.Add(_bookService.CreateAsync(Guid.NewGuid(), $"Title {i}", $"Description {i}", 
                     $"Author {i}", i + 100, $"House {i}", DateTime.UtcNow.AddDays(-i)));
+                tasks.Add(_eventService.CreateAsync(Guid.NewGuid(), $"Name {i}", $"Description {i}",
+                    DateTime.UtcNow.AddDays(-i), DateTime.UtcNow.AddDays(i)));
+                tasks.Add(_movieService.CreateAsync(Guid.NewGuid(), $"Title {i}", $"Description {i}",
+                    $"Director {i}", i + 110, DateTime.UtcNow.AddDays(-i)));
+                tasks.Add(_newspaperService.CreateAsync(Guid.NewGuid(), $"Title {i}", $"Description {i}",
+                    $"Type {i}", DateTime.UtcNow.AddDays(-i)));
             }
             await Task.WhenAll(tasks);
         }
